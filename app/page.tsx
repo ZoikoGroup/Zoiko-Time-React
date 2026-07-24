65a8cc4a92bffaa1fa44a28ac3bd98c37e4d0224
Niharika <Niharika@zoikogroup.com>
2026-07-24 17:22:39 +0530

    Added meta data for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+export const metadata = {
+  title: "ZoikoTime | AI Time Intelligence Platform",
+  description:
+"ZoikoTime is an AI time intelligence platform that helps enterprises understand work patterns, improve productivity, and support ethical workforce decisions."
+};
+
 import TimeHome from "./home/home";
 
 
